0f5df9c1c1413936ea28eea9ba23d8404879c4c0
Bogdan Myasnikov <voennyo@yandex.ru>
2025-03-30 15:16:31 +0300

    т

--- media/js/slider.js
+++ media/js/slider.js
@@ -1,14 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     const slides = document.querySelector('.slides');
-    const slide = document.querySelectorAll('.slide');
+    const slideItems = document.querySelectorAll('.slide');
     let currentIndex = 0;
-  
-    function showNextSlide() {
-      currentIndex = (currentIndex + 1) % slide.length;
-      const offset = -currentIndex * 100;
-      slides.style.transform = `translateX(${offset}%)`;
+    const totalSlides = slideItems.length;
+
+    function autoScroll() {
+        currentIndex = (currentIndex + 1) % totalSlides;
+        const offset = -currentIndex * 100;
+        slides.style.transform = `translateX(${offset}%)`;
     }
-  
-    setInterval(showNextSlide, 3000);
-  });
-  
+
+    // Медленное автоматическое переключение каждые 8 секунд
+    setInterval(autoScroll, 8000);
+});
